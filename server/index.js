8cf15450cada4b2ebe9b9e6eabf140afe4ee3a73
LucasAmmer <lucas@ammer.nl>
2023-07-22 23:16:19 +0200

    added error pages

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
-const { port, homeMSG, hashome } = require("../config/server.json");
+const { port, homeMSG, hashome, showerrors } = require("../config/server.json");
 
 app.get("/", (req, res) => {
   if (!hashome) {
-    res.status(404);
+    error(404, res);
     return;
   }
   res.send(
@@ -14,6 +14,23 @@ app.get("/", (req, res) => {
       : homeMSG
   );
 });
+
+const error = (code, res) => {
+  if (showerrors) {
+    if (code == 404) {
+      res.send("<h1>404</h1><hr><h3>url not found!</h3>");
+    } else if (code == 403) {
+      res.send(
+        "<h1>403</h1><hr><h2>Forbidden</h2><h3>You do not have acces to this url!</h3>"
+      );
+    } else {
+      res.send(`<h1>${code}</h1><hr><h3>Something went wrong!</h3>`);
+    }
+  } else {
+    res.status(code);
+  }
+  return;
+};
 
 app.listen(port, () => {
   console.log("started listing on port %d (http://localhost:%d)", port, port);
