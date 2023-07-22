327fa9d31b8c6956d2ca028eaa5593ec071670ea
LucasAmmer <lucas@ammer.nl>
2023-07-22 23:04:32 +0200

    added listner and implemented config

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
 const express = require("express");
+const app = express();
+
+const { port, homeMSG, hashome } = require("../config/server.json");
+
+app.get("/", (req, res) => {
+  res.send(
+    homeMSG == "dflt"
+      ? "<h1>easy api</h1><hr><h3>easy api default home page</h3>"
+      : homeMSG
+  );
+});
+
+app.listen(port, () => {
+  console.log("started listing on port %d (http://localhost:%d)", port, port);
+});
