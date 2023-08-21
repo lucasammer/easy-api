19f43ea9bb4785898546e8caef70956e4e947e92
LucasAmmer <lucas@ammer.nl>
2023-08-21 17:13:44 +0200

    added tests for index

---
+++ server/test/index.test.js
@@ -0,0 +1,28 @@
+let app;
+const axios = require("axios");
+
+describe("Testing server", () => {
+  it("starts up", (done) => {
+    let logs = [];
+    const logSpy = jest
+      .spyOn(global.console, "log")
+      .mockImplementation((msg) => {
+        logs.push(msg);
+      });
+
+    app = require("../index");
+
+    expect(logs).toContain("logging all traffic"); // Enabled by default
+    logSpy.mockRestore();
+    app = null;
+    done();
+  });
+
+  it("returns things", (done) => {
+    app = require("../index");
+    axios.get("http://localhost:8080").then((response) => {
+      expect(response.status).toBe(200);
+      done();
+    });
+  });
+});
